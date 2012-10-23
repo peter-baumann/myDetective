ae579c777be73066667bb4b7679a8a129d104ade
Sakari Koivunen <sakari.koivunen@emeca.fi>
2012-10-23 15:55:12 +0800

    Disable PDF parsing

--- NLP Project/src/Data.py
+++ NLP Project/src/Data.py
@@ -3,8 +3,8 @@ Created on 27.09.2012
 
 @author: Peter
 '''
-from src.parser.pdf2text import pdf2text
-from src.parser.text import textp
+#from src.parser.pdf2text import pdf2text
+from parser.text import textp
 
 class Data(object):
     '''
@@ -12,9 +12,12 @@ class Data(object):
     '''
 
     def __init__(self, src, limit = None):
-        if(len(src) > 4 and src[(len(src) - 4):(len(src))] == ".pdf"):
-            pdf_parser = pdf2text(src, limit)
-            self.text = pdf_parser.getText()
-        elif (len(src) > 4 and src[(len(src) - 4):(len(src))] == ".txt"):
+        if (len(src) > 4 and src[(len(src) - 4):(len(src))] == ".txt"):
             text_parser = textp(src, limit)
             self.text = text_parser.getText()
+        else:
+            print "parsing error at " + src
+        #elif(len(src) > 4 and src[(len(src) - 4):(len(src))] == ".pdf"):
+        #    pdf_parser = pdf2text(src, limit)
+        #    self.text = pdf_parser.getText()
+
